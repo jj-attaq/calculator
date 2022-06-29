88413894e04f7ffe3ac6dd7a7073498d411fd69e
jj.attaq <jj.attaq@tutanota.com>
2022-06-29 16:54:56 +0200

    basics in place

---
+++ src/script.js
@@ -0,0 +1,23 @@
+function operate(operator, a, b) {
+  return operator(a, b);
+}
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+
+
+
+// --------------------------------------------------
+function display(n) {
+  return document.querySelector('.display--content').textContent = `${n}`;
+}
