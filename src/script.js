df6d98a8977faeb3aad010d9e10814406aff2156
jj.attaq <jj.attaq@tutanota.com>
2022-07-02 02:54:59 +0200

    calculator calculates only 1 digit, clean code and fix later

--- src/script.js
+++ src/script.js
@@ -1,4 +1,20 @@
+"use strict";
+const init = null;
+let a = init;
+let b = init;
+let operator = init;
+
+// expand on below, much better than the other mess
+// needs to be integrated into the event creation functions below
+
+
 function operate(operator, a, b) {
+  if (operator === 'add') {operator = add}
+  if (operator === 'subtract') {operator = subtract}
+  if (operator === 'multiply') {operator = multiply}
+  if (operator === 'divide') {operator = divide}
+  
+  // if (operator === `${operator}`) {operator = operator}
   return operator(a, b);
 }
 function add(a, b) {
@@ -13,11 +29,46 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-
+function Expression(a, operator, b, result) {
+  this.a = a;
+  this.operator = operator;
+  this.b = b;
+  this.result = operate(operator, a, b)
+}
 
 
 
 // --------------------------------------------------
 function display(n) {
-  return document.querySelector('.display--content').textContent = `${n}`;
+  const dispVal = (n) => document.querySelector('.display--content').textContent = n;
+  return n.toString().length > 10 ? dispVal('ERROR') : dispVal(n);
 }
+const addButtonEv = () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('.btn--number')) {  
+      if (a, b, operator === null) {
+        a = +event.target.value;
+        return display(a);
+      } else if (operator != null) {
+        b = +event.target.value;
+        return display(b);
+      }
+    }
+    if (event.target.matches('.btn--operator')) {
+      if (a != null) {
+        operator = event.target.value;
+        console.log(event.target.value);
+      }
+    }
+  });
+};
+const addEqualsEv = () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('#btn-equals')) {
+      //return display(operate(operator, a, b));
+      return display(new Expression(a, operator, b).result);
+    }
+  })
+}
+addButtonEv();
+addEqualsEv();
