b5c3321f0d4520aa789e8513bc7a980b34e15552
jj.attaq <jj.attaq@tutanota.com>
2022-07-11 19:56:51 +0200

    serial operations and clear button implemented

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-// TEST
 "use strict";
 const init = null;
 let a = '';
@@ -11,30 +10,19 @@ function operate(operator, a, b) {
   if (operator === 'multiply') {operator = multiply}
   if (operator === 'divide') {operator = divide}
   
-  // if (operator === `${operator}`) {operator = operator}
   return operator(a, b);
 }
-function add(a, b) {
-  return +a + +b;
-}
-function subtract(a, b) {
-  return +a - +b;
-}
-function multiply(a, b) {
-  return +a * +b;
-}
-function divide(a, b) {
-  return +a / +b;
-}
-//const add = (a, b) => +a + +b; 
-//const subtract = (a, b) => +a - +b;
-//const multiply = (a, b) => +a * +b;
-//const divide = (a, b) => +a / +b;
+
+const add = (a, b) => a + b; 
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
 function Expression(a, operator, b, result) {
   this.a = a;
   this.operator = operator;
   this.b = b;
-  this.result = operate(operator, a, b)
+  this.result = operate(operator, +a, +b)
 }
 
 
@@ -45,33 +33,51 @@ function display(n) {
   // get rid of ERROR and put in rounding with Math.round
   return n.toString().length > 10 ? dispVal('ERROR') : dispVal(n);
 }
+function dispResult() {
+  a = new Expression(a, operator, b).result.toString();
+  return display(a);
+}
+function equalsReset() {
+  dispResult();
+  b = '';
+  operator = init;
+}
 const addButtonEv = () => {
   document.addEventListener('click', (event) => {
-    // rewrite so 5 * 4 + 1 * 2 = 42;
     if (event.target.matches('.btn--number')) {  
-      if (a, b, operator === null) {
+      if (operator === null) {
         a += event.target.value;
-        return display(+a);
+        return display(a);
       } else if (operator != null) {
         b += event.target.value;
         return display(b);
       }
     }
     if (event.target.matches('.btn--operator')) {
-      if (a != null) {
+        if (b.length > 0) {
+          equalsReset() 
+        }
         operator = event.target.value;
-        console.log(event.target.value);
-      }
     }
   });
 };
 const addEqualsEv = () => {
   document.addEventListener('click', (event) => {
     if (event.target.matches('#btn-equals')) {
-      //return display(operate(operator, a, b));
-      return display(new Expression(a, operator, b).result);
+      equalsReset() 
+    }
+  })
+}
+const clearBtnEv = () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('#btn-clear')) {
+      a = '';
+      b = '';
+      operator = init;
+      display('');
     }
   })
 }
 addButtonEv();
 addEqualsEv();
+clearBtnEv();
